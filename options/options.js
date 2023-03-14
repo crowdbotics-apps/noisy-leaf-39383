328edc886f1c56f96f3918b8c894129996c45ee4
Crowdbotics <team@crowdbotics.com>
2023-03-14 21:15:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-leaf-39383",
+  url: "https://noisy-leaf-39383.botics.co",
+  api: "https://noisy-leaf-39383.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
